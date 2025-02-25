d92d26c596dfb1d4132a28330701967e4d7e71c9
Georgi <georgiasandei@gmail.com>
2025-02-25 11:06:32 +0200

    uploaded first lab at MPP

--- module/src/abc.java
+++ module/src/abc.java
@@ -1,6 +1,6 @@
 public class abc {
     public static void main(String[] args) {
         System.out.printf("Test pentru GitHub!\n");
-        System.out.printf("Inca un mesaj ca sa fie");
+        System.out.printf("Inca un mesaj ca sa fie :)");
     }
 }
